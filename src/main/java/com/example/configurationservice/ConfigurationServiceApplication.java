190372e81d69de543b45e2239a749c64ac152cc1
Josh Long <josh@joshlong.com>
2020-11-02 18:41:24 -0800

    cs

--- src/main/java/com/example/configurationservice/ConfigurationServiceApplication.java
+++ src/main/java/com/example/configurationservice/ConfigurationServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class ConfigurationServiceApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationServiceApplication.class, args);
 	}
