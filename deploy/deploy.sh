0e315bdf3f36a3c5ea175122e4ef8dfe3d3d55b0
Josh Long <josh@joshlong.com>
2020-11-04 04:39:35 -0800

    build

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-
-
 export APP_NAME=${APP_NAME:-configuration}
 ROOT_DIR=$(cd $(dirname $0)/.. && pwd)
 APP_YAML=${ROOT_DIR}/deploy/bp-configuration.yaml
 APP_SERVICE_YAML=${ROOT_DIR}/deploy/bp-configuration-service.yaml
 SECRETS=${APP_NAME}-secrets
-
 
 kubectl delete secrets ${SECRETS} || echo "could not delete ${SECRETS}."
 kubectl delete -f $APP_YAML || echo "could not delete the existing Kubernetes environment as described in ${APP_YAML}."
@@ -26,6 +23,7 @@ stringData:
   SPRING_PROFILES_ACTIVE: cloud
 ")
 
-kubectl apply -f $APP_YAML
 
+## todo need some way to parameterize this yaml for the DNS URI
+kubectl apply -f $APP_YAML
 kubectl get service | grep $APP_NAME || kubectl apply -f $APP_SERVICE_YAML
