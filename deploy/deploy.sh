89b8986780b48ecde89f0b7ff84c3eab40e57f77
Josh Long <josh@joshlong.com>
2020-11-30 04:00:27 -0800

    kustomize

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
 export ROOT_DIR=$(cd $(dirname $0) && pwd)
-
-
 export BP_MODE_LOWERCASE=${BP_MODE_LOWERCASE:-development}
 export OD=${ROOT_DIR}/overlays/${BP_MODE_LOWERCASE}
-
-
 export APP_NAME=configuration
 export SECRETS=${APP_NAME}-secrets
 export SECRETS_FN=${OD}/${APP_NAME}-secrets.env
 
-mkdir -p `dirname $SECRETS_FN`
+mkdir -p $(dirname $SECRETS_FN)
 touch $SECRETS_FN
 
 export RESERVED_IP_NAME=configuration-${BP_MODE_LOWERCASE}-ip
-gcloud compute addresses list --format json | jq '.[].name' -r | grep $RESERVED_IP_NAME || \
-    gcloud compute addresses create $RESERVED_IP_NAME --global
+gcloud compute addresses list --format json | jq '.[].name' -r | grep $RESERVED_IP_NAME ||
+  gcloud compute addresses create $RESERVED_IP_NAME --global
 
 echo writing to "$SECRETS_FN "
 cat <<EOF >${SECRETS_FN}
@@ -27,8 +23,6 @@ CONFIGURATION_SERVER_PASSWORD=${CONFIGURATION_SERVER_PASSWORD}
 SPRING_PROFILES_ACTIVE=cloud
 EOF
 
-ls -la $SECRETS_FN && cat $SECRETS_FN
-
 kubectl apply -k ${OD} --prune --all
 
-#rm $SECRETS_FN
+rm $SECRETS_FN
