6a5f1a9994ce84bac7db7204480f540df7b0ebdc
Josh Long <josh@joshlong.com>
2020-12-01 21:23:09 -0800

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,8 +9,8 @@ export SECRETS_FN=${OD}/${APP_NAME}-secrets.env
 
 mkdir -p $(dirname $SECRETS_FN)
 touch $SECRETS_FN
-
-export RESERVED_IP_NAME=configuration-${BP_MODE_LOWERCASE}-ip
+export BP_MODE_LOWERCASE=development
+export RESERVED_IP_NAME=${BP_MODE_LOWERCASE}-ip
 gcloud compute addresses list --format json | jq '.[].name' -r | grep $RESERVED_IP_NAME ||
   gcloud compute addresses create $RESERVED_IP_NAME --global
 
